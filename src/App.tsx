fe25393b0bda950e14e8a9f4195b196619b185c7
Oleksandr <lysakova@yahoo.com>
2023-07-22 17:29:09 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './styles/App.css';
+
+const App = () => {
+  return (
+    <>
+      <div className='container'>
+        <h1>pomodoro</h1>
+        
+      </div>
+    </>
+  );
+};
+
+export default App;
